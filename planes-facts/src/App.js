0834c3b3b5a2f219d6d87612f61f2ff2288e64f0
Alm1ra <frydlewicz.ola@gmail.com>
2023-08-25 23:38:58 +0200

    Created a new component and added some data

--- planes-facts/src/App.js
+++ planes-facts/src/App.js
@@ -1,24 +1,37 @@
-import logo from './logo.svg';
 import './App.css';
+import Card from './components/Card'
 
 function App() {
+
+  const facts = [
+    {
+      title: 'Supersonic Speedsters',
+      image: 'aaaa',
+      description: 'Fighter planes are known for their blistering speed. The fastest fighter, the North American X-15, reached a mind-boggling speed of over 4,500 mph (7,270 km/h).',
+    },
+    {
+      title: 'Dogfight Legends',
+      image: 'bbbb',
+      description: 'The term "dogfight" originated from World War I aerial combat, where pilots maneuvered like dogs chasing their tails. Modern technology has changed the nature of air combat, but the term endures.',
+    },
+    {
+      title: 'G-Force Endurance',
+      image: 'cccc',
+      description: 'Fighter pilots endure extreme G-forces during maneuvers. The human body can handle up to 9 Gs, but with specialized suits and training, pilots can push this limit.',
+    },
+    {
+      title: 'Vertical Takeoff Marvels',
+      image: 'dddd',
+      description: 'Vertical takeoff and landing (VTOL) fighters, like the Harrier Jump Jet, can lift off and land vertically, enabling them to operate from confined spaces or even amphibious assault ships.',
+    }
+  ];
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <Card>
+      <header>
+        <h1>Unveiling Fighter Plane Facts</h1>
       </header>
-    </div>
+    </Card>
   );
 }
 
